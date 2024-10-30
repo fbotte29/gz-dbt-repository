d65d96ec217db2c88471a3d4ce9a4153f3e1f688
fbo <frbotte@protonmail.com>
2024-10-30 15:24:33 +0000

    int_sales_margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,32 +1,10 @@
-With 
-
-ref AS (
-
-select orders_id,date_date,quantity,products_id,revenue,purchase_price,products_id from {{ ref('stg_raw__sales')}} s
-LEFT JOIN {{ ref('stg_raw__product') }} p
-ON s.product_id = p.products_id
-),
-
-pcost as (
-
-    select
-        orders_id,
-        date_date,
-        revenue,
-        quantity,
-        purchase_price,
-        (quantity* purchase_price) AS purchase_cost
-
-    from ref
-    
-)
-select
+SELECT
 orders_id,
 date_date,
-ROUND(SUM(revenue),2) AS revenue,
-ROUND(SUM(quantity),2) AS quantity,
-ROUND(SUM(purchase_cost),2) AS purchase_cost,
-ROUND(SUM(revenue - purchase_cost),2) AS margin
-from pcost
-group by orders_id,date_date
-order by orders_id DESC
+ROUND(SUM(revenue),2) as revenue,
+ROUND(SUM(quantity),2) as quantity,
+ROUND(SUM(purchase_cost),2) as purchase_cost,
+ROUND(SUM(margin),2) as margin
+FROM {{ ref("int_sales_margin") }}
+GROUP BY orders_id,date_date
+ORDER BY orders_id DESC
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,26 +1,12 @@
-With 
-
-ref AS (
-
-select quantity,products_id,revenue,purchase_price,products_id from {{ ref('stg_raw__sales')}} s
-LEFT JOIN {{ ref('stg_raw__product') }} p
-ON s.product_id = p.products_id
-),
-
-pcost as (
-
-    select
-        revenue,
-        purchase_price,
-        quantity,
-        (quantity* purchase_price) AS purchase_cost
-
-    from ref
-    
-)
-select
-purchase_price,
-quantity,
-purchase_cost,
-ROUND((revenue - purchase_cost),2) AS margin
-from pcost
+  SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+  ON s.product_id = p.products_id
