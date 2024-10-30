0973d80544c27a6f04ad2b17bf8f8fa8df2ec9a2
fbo <frbotte@protonmail.com>
2024-10-30 10:32:00 +0000

    add margin calculation

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -2,7 +2,7 @@ With
 
 ref AS (
 
-select quantity,products_id,purchase_price,products_id from {{ ref('stg_raw__sales')}} s
+select quantity,products_id,revenue,purchase_price,products_id from {{ ref('stg_raw__sales')}} s
 LEFT JOIN {{ ref('stg_raw__product') }} p
 ON s.product_id = p.products_id
 ),
@@ -10,6 +10,7 @@ ON s.product_id = p.products_id
 pcost as (
 
     select
+        revenue,
         purchase_price,
         quantity,
         (quantity* purchase_price) AS purchase_cost
@@ -17,5 +18,11 @@ pcost as (
     from ref
     
 )
+select
+purchase_price,
+quantity,
+purchase_cost,
+ROUND((revenue - purchase_cost),2) AS margin
+from pcost
 
-select purchase_price,quantity,purchase_cost from pcost
+-- select purchase_price,quantity,purchase_cost from pcost
